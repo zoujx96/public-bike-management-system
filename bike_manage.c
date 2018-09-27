2186c41412015325f27bc31322e6a01557ebfe37
Jiaxu Zou <zoujx96@gmail.com>
2018-09-27 01:30:42 -0400

    Update bike_manage.c

--- bike_manage.c
+++ bike_manage.c
@@ -55,9 +55,11 @@ void CheckGood();
 
 int main() {
 	int v, w, i, j, tem, path[512];
+	/*Type in the capacity of vertex, number of vertexes, destination vertex and number of edges*/
 	scanf("%d %d %d %d", &Full, &N, &sp, &M);
 	Half = Full / 2;
-	for (i = 1; i <= N; i++) {  
+	for (i = 1; i <= N; i++) {
+		/*Type in the current bike number of each vertex*/
 		scanf("%d", &cur[i]);
 		if (cur[i] > Full) {
 			printf("ERROR INPUT\n");
@@ -66,6 +68,7 @@ int main() {
 		dist[i] = -1;
 		known[i] = 0;
 		p[i] = 0;
+		/*Initialization for path notes*/
 		for (j = 1; j <= N; j++) {
 			G[i][j] = 0;
 			GNode[i][j].Ne = GNode[i][j].Nv = 0;
@@ -73,6 +76,7 @@ int main() {
 		} 
 	}
 	for (i = 0; i < M; i++) {
+		/*Type in the undirected graph information*/
 		scanf("%d %d %d", &v, &w, &tem);
 		if (v > N || w > N || w==v||(G[v][w] != 0 && G[v][w] != tem)) {
 			printf("ERROR INPUT\n");
@@ -80,11 +84,14 @@ int main() {
 		}
 		G[v][w] = G[w][v] = tem;    
 	}
+	/*Find all the shortest paths*/
 	ShortestDist(sp);
+	
 	if (p[sp] == 0) {
 		printf("Can't find one way to the target\n");
 		system("pause");
 	}
+	/*Choose the best shortest path with the least bikes to send or take back*/
 	CheckGood();
 	system("pause");
 	return 0;
@@ -100,6 +107,7 @@ void ShortestDist(int x) {
 		len = INFINITY;
 		v = -1;
 		for (i = 0; i <= N; i++) {
+			/*Have reached the vertex already, so skip*/
 			if (known[i]) continue;
 			if (dist[i] >= 0 && dist[i] < len) {
 				len = dist[i];
@@ -107,34 +115,45 @@ void ShortestDist(int x) {
 			}
 		} 
 		if (v == -1) break;
+		/*Mark as reached*/
 		known[v] = 1;
 		for (w = 1; w <=N; w++) {
+			/*Have reached or can't reach, so skip*/
 			if (!G[v][w] || known[w]) continue; 
+			/*Not a shorter path, so skip*/
 			if (dist[w] > 0 && dist[w] < dist[v] + G[v][w]) continue;
 			else if (dist[w] == -1 || dist[w] >= dist[v] + G[v][w]) {
+				/*Have found a shorter path, so all the shortest paths found before are invalid*/
 				if (dist[w] == -1 || dist[w] > dist[v] + G[v][w]) {
 					p[w] = 0;  
 				}
+				/*Update the path length*/
 				dist[w] = dist[v] + G[v][w];
 				temp = cur[w] > Half ? cur[w] - Half : Half - cur[w];
 				for (i = 0, j = p[w]; i < p[v]; i++, j++) {
 					GNode[w][j].Nv = v;
 					GNode[w][j].Ne = i;
+					/*The number of bikes which go via the vertex is bigger than half the capacity*/
 					if (cur[w] >= Half) { 
+						/*The number of bikes to send back becomes more*/
 						GNode[w][j].Back = GNode[v][i].Back + temp;  
+						/*The number of bikes to send out remains the same*/
 						GNode[w][j].Send = GNode[v][i].Send; 
 					}
 					else {
+						/*The number of bikes is smaller than half and no more than the bikes left before*/
 						if (temp <= GNode[v][i].Back) {
 							GNode[w][j].Back = GNode[v][i].Back - temp;
 							GNode[w][j].Send = GNode[v][i].Send;
 						}
+						/*Increase the bikes to send out*/
 						else {
 							GNode[w][j].Send = GNode[v][i].Send + temp - GNode[v][i].Back;
 							GNode[w][j].Back = 0;
 						}
 					}
 				}
+				/*Update the number of shortest paths to the current vertex*/
 				p[w] = j;
 			}
 		}
@@ -145,6 +164,7 @@ void CheckGood() {
 	int i, j, k;
 	int send, back, temp1,temp2,path[MAXVertex];
 	send = back = INFINITY;
+	/*Compare between different shortest paths. Choose the path with least bikes to send out in priority. If there are multiple choices, then choose the one with least bikes to send back.*/
 	for (i = 0; i < p[sp]; i++) {
 		if (send > GNode[sp][i].Send || (send == GNode[sp][i].Send&&back > GNode[sp][i].Back)) {
 			send = GNode[sp][i].Send;
@@ -156,6 +176,7 @@ void CheckGood() {
 	i = temp1;
 	j = temp2;
 	k = 0;
+	/*Trace back the best shortest path and print it out*/
 	while (i >= 0) { 
 		path[k++] = i;
 		temp1 = GNode[i][j].Nv;
